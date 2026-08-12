1a50306fbe1aa41c71af657b02e97365d8f10faf
jsagitariom619 <jsagitariom619@gmail.com>
2026-08-12 15:16:27 -0400

    fix: renovar cache tras correccion historica

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="footballhub-v5";
+const CACHE="footballhub-v6";
 const ASSETS=["./","./index.html","./css/main.css","./js/app.js","./js/ui/ui.js","./js/data/competitions.js","./js/data/teams.js","./js/data/matches.js","./js/modules/dashboard.js","./js/modules/competitions.js","./js/modules/competition-history.js","./js/modules/teams.js","./js/modules/team-analysis.js","./js/modules/historical.js","./js/services/historical-data.js","./js/services/storage.js","./manifest.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))).then(()=>self.clients.claim()))));
